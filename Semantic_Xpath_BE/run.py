161b3114e5e703e4d1340005efd05e2403883323
RuifanRickyWu <ruifan.wu@mail.utoronto.ca>
2026-02-19 16:58:05 -0500

    change be startup config to run the app in container

--- Semantic_Xpath_BE/run.py
+++ Semantic_Xpath_BE/run.py
@@ -13,10 +13,9 @@ sys.path.insert(0, str(Path(__file__).parent))
 
 from app_factory import create_app
 
+app = create_app()
 
 if __name__ == "__main__":
     import os
-
-    app = create_app()
     port = int(os.environ.get("PORT", 5001))
-    app.run(host="0.0.0.0", port=port, debug=os.environ.get("FLASK_DEBUG", "0") == "1")
+    app.run(host="0.0.0.0", port=port)
